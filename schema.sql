14c34c9a003e1bc79ee3e6641e61122aa55aad1e
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-07 11:13:43 -0700

    updated schema

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ USE reservations_db;
 CREATE TABLE reserve (
   name VARCHAR(100) NOT NULL,
   phone INT(20) NOT NULL,
-  email VARCHAR NOT NULL,
+  email VARCHAR(50) NOT NULL,
   uniqueID INT AUTO_INCREMENT NOT NULL,
   PRIMARY KEY (uniqueID)
 );
